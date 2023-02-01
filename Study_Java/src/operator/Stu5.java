ab37df477f106034b1a9f53a6477859e3ba4f96f
woodawon <woodawon0602@gmail.com>
2023-02-01 23:21:47 +0900

    2023.02.01

--- Study_Java/src/operator/Stu5.java
+++ Study_Java/src/operator/Stu5.java
@@ -4,8 +4,16 @@ public class Stu5 {
 
 	public static void main(String[] args) {
 
-		System.out.println("hello");
-
+		int num = 0;
+		System.out.println((num > 0) ?"양수" : (num < 0)  ?"음수" : "0"); // 양수, 음수, 0 판별 코드 -> 삼항 연산자
+		
+		
+		int fahrenheit = 100;
+		float celcius = (float)5/9 * (fahrenheit - 32);
+		
+		System.out.println(fahrenheit);
+		System.out.println(String.format("%.2f", celcius)); // Math.round() 외의 다른 방식인 반올림 방법임.
+		
 	}
 
 }
