38bf6920b6ee93050660e3a2b22d9f45f49d6205
woodawon <woodawon0602@gmail.com>
2023-02-04 23:32:42 +0900

    2023.02.04

---
+++ Study_Java/src/if_loop/Stu6.java
@@ -0,0 +1,37 @@
+package if_loop;
+
+import java.util.Scanner;
+
+public class Stu6 {
+
+	public static void main(String[] args) {
+
+		System.out.println("원하는 월을 입력하세요.");
+		
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		
+		switch (month) {
+			case 3: case 4: case 5: {
+				System.out.println("봄이네요.");
+				break;
+			}
+			case 6: case 7: case 8: {
+				System.out.println("여름이네요.");
+				break;
+			}
+			case 9: case 10: case 11: {
+				System.out.println("가을이네요.");
+				break;
+			}
+			case 12: case 1: case 2: {
+				System.out.println("겨울이네요.");
+				break;
+			}
+			default:
+				System.out.println("월에 해당하는 수가 맞나요?");
+			}
+
+	    }
+
+}
---
+++ Study_Java/src/if_loop/Stu7.java
@@ -0,0 +1,20 @@
+package if_loop;
+
+import java.util.Iterator;
+
+public class Stu7 {
+
+	public static void main(String[] args) {
+
+		Loop1 : for (int i = 2; i <= 9; i++) {
+			for(int j = 1;j <= 9;j++) {
+				if(j == 5) {
+					break Loop1;
+				}
+				System.out.println(i + "*" + j + "=" + i*j);
+			}
+		}
+
+	}
+
+}
