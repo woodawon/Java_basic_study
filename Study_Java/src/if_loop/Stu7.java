d341cce5c1a341d02c3b6776d650689ee42936f5
우다원 <105121244+woodawon@users.noreply.github.com>
2023-02-05 23:28:15 +0900

    Update Stu7.java

--- Study_Java/src/if_loop/Stu7.java
+++ Study_Java/src/if_loop/Stu7.java
@@ -6,10 +6,10 @@ public class Stu7 {
 
 	public static void main(String[] args) {
 
-		Loop1 : for (int i = 2; i <= 9; i++) {
+		Loop1 : for (int i = 2; i <= 9; i++) { // for문의 이름을 정해준 것
 			for(int j = 1;j <= 9;j++) {
 				if(j == 5) {
-					break Loop1;
+					break Loop1; 
 				}
 				System.out.println(i + "*" + j + "=" + i*j);
 			}
