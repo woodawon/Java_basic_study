1e895a6c6c5fe31eeb4905ec25e2b0cd73ca3bda
woodawon <woodawon0602@gmail.com>
2023-02-10 23:36:13 +0900

    dd

--- Study_Java/src/array/Stu8.java
+++ Study_Java/src/array/Stu8.java
@@ -1,12 +1,34 @@
 package array;
 
+import java.util.Scanner;
+
 public class Stu8 {
 
 	public static void main(String[] args) {
 
-		char[] ex = {'1', '2', '3'};
-		System.out.println(ex);
-		System.out.println(ex[1]);
+		String[] words = {"apple", "banana", "cookie"};
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for (int i = 0; i < words.length; i++) {
+			char[] question = words[i].toCharArray();
+			
+			char tmp = question[i];
+			question[i] = question[i + 1];
+			question[i + 1] = tmp;
+			
+			System.out.printf("Q.%d %s의 정답을 입력하세요. : ", i + 1, new String(question));
+			String answer = scanner.nextLine();
+			
+			if (words[i].equals(answer.trim())) {
+				System.out.println("맞았어요.\n");
+			}
+			else {
+				System.out.println("틀렸어요.\n");
+			}
+			
+		}
+		scanner.close();
 	
 
 	}
